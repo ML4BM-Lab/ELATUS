642841b47b432041d6db0b4f19da85c237fcad1c
kikegoni <egecheverria_1993@hotmail.com>
2024-04-09 10:32:44 +0200

    ELATUS working also with preprocessed objects

--- R/ELATUS_filtered.r
+++ R/ELATUS_filtered.r
@@ -1,14 +1,8 @@
 #' ELATUS workflow
 #' This workflow loads the Kallisto and CellRanger raw count matrices, performs emptydrops, then doublet removal, a quality control filtering. Next it gets the highly expressed lncRNAs only detected by Kallisto and the ratio of their expression between Kallisto/CellRanger and finally, after normalization and clustering it calculates the Specificity Index of each of them. The last step is generating the list of biologically relevant lncRNAs. In addition it identifies highly-expressed lncRNAs exclusively detected by Kallisto whose functionality has been proven by external biobliography (CRISPR screenings...) and those highly-expressed lncRNAs robustly detected by both Cell Ranger and Kallisto.
-#' @param kallisto_path Path to the Kallisto raw count matrix (As an example: kallisto_path=system.file("extdata", "kallisto_example_raw_matrix", package = "ELATUS"))
-#' @param kallisto_name Name of the Kallisto raw count matrix (As an example kallisto_name="cells_genes_NO_multimapping")
-#' @param cellRanger_path Path to the CellRanger raw count matrix (As an example :cellRanger_path=system.file("extdata", "cellRanger_example_raw_matrix", package = "ELATUS"))
 #' @param organism Human or Mouse dataset (for the example "Mouse") 
-#' @param lower_emptydrops A numeric scalar specifying the lower bound on the total UMI count, at or below which all barcodes are assumed to correspond to empty droplets. For the example, 1000
-#' @param EmptyDrops_FDR_thres FDR threshold to classifly empty droplets (For the example, 0.01)
-#' @param cells_mito_threshold Mitochondrial content (%) threshold. Keep cells with less mitocondrial content than the defined threshold (For the example = 15)
-#' @param cells_max_threshold Keep cells with less counts than the defined threshold (For the example = 30000)
-#' @param cells_min_genes_detected_threshold Keep cells with counts in more genes than the defined threhold (For the example = 500)
+#' @param kallisto_sce Kallisto SingleCellExperiment object
+#' @param cellRanger_path CellRanger SingleCellExperiment object
 #' @param threshold_minumun_gene_counts Select genes with more than this total counts  (For the paper = 250 counts)
 #' @param threshold_cells_detected Select genes present in at least a number of cells higher than this threshold (For the paper = 25)
 #' @param dimred_clustering dimensionality reduction (For the example "PCA")
@@ -19,7 +13,7 @@
 #' @return A list with most biologically relevant lncRNAs
 
 #' @export
-ELATUS <- function(kallisto_path, kallisto_name, cellRanger_path, organism, lower_emptydrops, EmptyDrops_FDR_thres, cells_mito_threshold, cells_max_threshold, cells_min_genes_detected_threshold, threshold_minumun_gene_counts, threshold_cells_detected, dimred_clustering, k_neighbors, ratio_threshold, CR_threshold, SI_threshold)
+ELATUS_filtered <- function(organism, kallisto_sce, cellRanger_sce, threshold_minumun_gene_counts, threshold_cells_detected, dimred_clustering, k_neighbors, ratio_threshold, CR_threshold, SI_threshold)
 {
     if (organism == "Human")
     {
@@ -32,36 +26,10 @@ ELATUS <- function(kallisto_path, kallisto_name, cellRanger_path, organism, lowe
     crispr_data <- readRDS(system.file("extdata", "hits_info_Liu_science_2015_ensids.rds", package = "ELATUS"))
     gtf <- readRDS(gencode_path)
     gtf$gene_id <- gsub("_","-",gtf$gene_id)
-    mitochondrial_ens_ids <- unique(gtf$gene_id[grep("^MT-",gtf$gene_name)])
     lncrna_ens_ids <- unique(c(gtf$gene_id[grep("lncRNA",gtf$gene_type)]))
     protein_coding_ens_ids <- unique(c(gtf$gene_id[gtf$gene_type=="protein_coding"]))
     lncrna_names <- unique(gtf$gene_name[gtf$gene_id %in% lncrna_ens_ids])
     protein_coding_names <-  unique(gtf$gene_name[gtf$gene_id %in% protein_coding_ens_ids])
-
-    kallisto <- import_kallisto_sc(kallisto_path, kallisto_name)
-    kallisto <- Seurat::as.SingleCellExperiment(kallisto)
-    kallisto_sce <- qc_metrics(kallisto, mitochondrial_ens_ids)
-
-    cellRanger <- import_CellRanger_sc(cellRanger_path)
-    cellRanger <- Seurat::as.SingleCellExperiment(cellRanger)
-    cellRanger_sce <- qc_metrics(cellRanger, mitochondrial_ens_ids)
-
-    #EmptyDrops filtering
-    #for the example given (human PBMCs) the parameters are lower_emptydrops=1000 & EmptyDrops_FDR_thres = 0.01
-    kallisto_filt_sce_ed <- emptydrops_filt(kallisto_sce, lower = lower_emptydrops, EmptyDrops_FDR_thres = EmptyDrops_FDR_thres)
-    cellRanger_filt_sce_ed <- emptydrops_filt(cellRanger_sce, lower = lower_emptydrops, EmptyDrops_FDR_thres = EmptyDrops_FDR_thres)
-
-    # Remove doublets
-    kallisto_filt_sce_ed_nodoubs <- remove_doublets(kallisto_filt_sce_ed)
-    kallisto_filt_sce_ed_nodoubs <- kallisto_filt_sce_ed_nodoubs[,kallisto_filt_sce_ed_nodoubs$isDoublet == F]
-    cellRanger_filt_sce_ed_nodoubs <- remove_doublets(cellRanger_filt_sce_ed)
-    cellRanger_filt_sce_ed_nodoubs <- cellRanger_filt_sce_ed_nodoubs[,cellRanger_filt_sce_ed_nodoubs$isDoublet == F]
-
-    # Filtering (The following thresholds have been used. cells_mito_threshold=15, cells_max_threshold= 50000, cells_min_genes_detected_threshold = 500)cells_mito_threshold, cells_max_threshold, cells_min_genes_detected_threshold
-    kallisto_filt_sce <- Filtering(kallisto_filt_sce_ed_nodoubs,  cells_mito_threshold=cells_mito_threshold, cells_max_threshold= cells_max_threshold, cells_min_genes_detected_threshold = cells_min_genes_detected_threshold)
-    kallisto_filt_sce <- scuttle::logNormCounts(kallisto_filt_sce)
-    cellRanger_filt_sce <- Filtering(cellRanger_filt_sce_ed_nodoubs,  cells_mito_threshold=cells_mito_threshold, cells_max_threshold= cells_max_threshold, cells_min_genes_detected_threshold = cells_min_genes_detected_threshold)
-    cellRanger_filt_sce <- scuttle::logNormCounts(cellRanger_filt_sce)
 
     # Now get the highly expressed lncRNAs only detected by Kallisto and the ratio of their expression between Kallisto/CellRanger. Also get the highly-expressedcommonly detected by Cell Ranger and Kallisto
     # uniquifyFeatures
@@ -71,7 +39,6 @@ ELATUS <- function(kallisto_path, kallisto_name, cellRanger_path, organism, lowe
     rownames(cellRanger_filt_sce) <- scuttle::uniquifyFeatureNames(rownames(cellRanger_filt_sce), gene_name)
 
     # We considered highly expressed lncRNAs as those with at least 250 counts in at least 25 cells
-
     top_genes(kallisto_filt_sce,threshold_minumun_gene_counts,threshold_cells_detected)
     candidate_lncRNAs_exclusive <- get_candidates(kallisto_filt_sce, cellRanger_filt_sce , threshold_minumun_gene_counts = threshold_minumun_gene_counts, threshold_cells_detected = threshold_cells_detected,lncrna_names = lncrna_names,gtf=gtf)
     candidate_lncRNAs_common <- get_candidates(kallisto_filt_sce, cellRanger_filt_sce, threshold_minumun_gene_counts = threshold_minumun_gene_counts, threshold_cells_detected = threshold_cells_detected,lncrna_names = lncrna_names,gtf=gtf, exclusive = F)
@@ -96,9 +63,6 @@ ELATUS <- function(kallisto_path, kallisto_name, cellRanger_path, organism, lowe
     candidate_lncRNAs_common$cell_type_SI <- candidate_lncRNAs_common$cell_type_SI <- colnames(counts_cell_specificity_index[rownames(candidate_lncRNAs_common),])[apply(counts_cell_specificity_index[rownames(candidate_lncRNAs_common),],1,which.max)]
 
     # Get the biologically relevant lncRNAs from these candidates (In the paper we used the following parameters: ratio_threshold = 40, CR_threshold = 10, SI_threshold = 0.15)
-    ratio_threshold = 40
-    CR_threshold = 10
-    SI_threshold = 0.15
     exclusive_lncRNAs_CRISPRi <- candidate_lncRNAs_exclusive[candidate_lncRNAs_exclusive$crispr_intersection == T,]
     exclusive_biologically_relevant_lncRNAs <- biologically_relevant_lncRNAs(candidate_lncRNAs_exclusive, ratio_threshold,CR_threshold,SI_threshold)
     candidate_lncRNAs_common$crispr_intersection = "NA"

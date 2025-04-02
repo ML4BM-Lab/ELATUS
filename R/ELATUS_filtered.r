8f03a1c858601422d64c1ac7b938473c839631c9
kikegoni <egecheverria_1993@hotmail.com>
2025-04-02 11:35:47 +0200

    adding parameter cell_types_defined to indicate that cells are already classified and there is not needed to cluster them

--- R/ELATUS_filtered.r
+++ R/ELATUS_filtered.r
@@ -6,6 +6,8 @@
 #' @param gene_names Are gene names the rownames of the SingleCellExperiment object
 #' @param threshold_minumun_gene_counts Select genes with more than this total counts  (For the paper = 250 counts)
 #' @param threshold_cells_detected Select genes present in at least a number of cells higher than this threshold (For the paper = 25)
+#' @param cell_types_defined Are cells already assigned to its corresponding cell type/subtype/class? In case they're,it is not necessary to perform the clustering(For the paper = FALSE)
+#' @param cell_type_class If cell_types_defined is TRUE, indicate de metadata name of the cell types colData (Ex: "cell_type")
 #' @param dimred_clustering dimensionality reduction (For the example "PCA")
 #' @param k_neighbors the number of nearest neighbors used to construct the graph. Choose a smaller k to more but smaller clusters as lncRNAs tend to be expressed in small subpopulations. (NA when the object is already clustered)
 #' @param ratio_threshold Threshold to remove lncRNAs whose ratio of expression between Kallisto and CellRanger is smaller than this defined threshold (For the paper = 40)
@@ -13,7 +15,7 @@
 #' @param SI_threshold Threshold to remove lncRNAs whose SI is smaller than this defined threshold (For the paper = 0.15)
 #' @return A list with most biologically relevant lncRNAs
 
-#' @export
+#' @export 
 ELATUS_filtered <- function(organism, kallisto_sce, cellRanger_sce, gene_names,threshold_minumun_gene_counts, threshold_cells_detected, dimred_clustering, k_neighbors, ratio_threshold, CR_threshold, SI_threshold)
 {
     if (organism == "Human")
@@ -52,18 +54,26 @@ ELATUS_filtered <- function(organism, kallisto_sce, cellRanger_sce, gene_names,t
     candidate_lncRNAs_exclusive <- get_candidates(kallisto_filt_sce, cellRanger_filt_sce , threshold_minumun_gene_counts = threshold_minumun_gene_counts, threshold_cells_detected = threshold_cells_detected,lncrna_names = lncrna_names,gtf=gtf)
     candidate_lncRNAs_common <- get_candidates(kallisto_filt_sce, cellRanger_filt_sce, threshold_minumun_gene_counts = threshold_minumun_gene_counts, threshold_cells_detected = threshold_cells_detected,lncrna_names = lncrna_names,gtf=gtf, exclusive = F)
 
-    # clustering
-    set.seed(100100100)
-    if (is.na(k_neighbors)==FALSE)
+    if (cell_types_defined == FALSE)
     {
+        # clustering
+        set.seed(100100100)
+        if (is.na(k_neighbors)==FALSE)
+        {
         kallisto_filt_sce <- scater::runPCA(kallisto_filt_sce) 
         g <- scran::buildSNNGraph(kallisto_filt_sce, use.dimred = dimred_clustering, k = k_neighbors ) # k is the number of nearest neighbors used to construct the graph. Choose a smaller k to more but smaller clusters as lncRNAs tend to be expressed in small subpopulations. (in this example, k=5). dimred_clustering is the dimensionality reduction (PCA here, but could be the corrected space after integrating samples)
         clust <- igraph::cluster_louvain(g)$membership
         print(table(clust))
         kallisto_filt_sce$louvain_clusters <- factor(clust)
+        }
+        # Calculate the Specificity Index for each gene
+        SI <- SI(kallisto_filt_sce,group_by="louvain_clusters", average_by="mean")
     }
-    # Calculate the Specificity Index for each gene
-    SI <- SI(kallisto_filt_sce,group_by="louvain_clusters", average_by="mean")
+
+    if (cell_types_defined == TRUE)
+    {
+        SI <- SI(kallisto_filt_sce,group_by=cell_type_class, average_by="mean")
+    }
     cell_type_specific_score <- SI[["cell_type_specificity_score"]]
     counts_cell_specificity_index <- SI[["counts_cell_specificity_index"]]
     candidate_lncRNAs_exclusive$SI <- cell_type_specific_score[rownames(candidate_lncRNAs_exclusive)]
